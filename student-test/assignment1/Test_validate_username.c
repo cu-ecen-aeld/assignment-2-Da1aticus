adc24ee8648922843bdbdba37bb7b01a557e34c7
Da1aticus <cvenkel.domen@gmail.com>
2024-03-10 14:29:15 +0100

    Assignment1: accidentally deleted include files

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -1,3 +1,9 @@
+#include "unity.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include "../../examples/autotest-validate/autotest-validate.h"
+#include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
+
 /**
 * This function should:
 *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
